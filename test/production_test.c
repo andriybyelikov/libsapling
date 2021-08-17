8e2632bb2dc2ad5df92ddca338bc3fbf0388237c
Andriy Byelikov <andriybyelikov@gmail.com>
2021-08-17 21:46:43 +0200

    Add test for cc/production.h

---
+++ test/production_test.c
@@ -0,0 +1,22 @@
+#include <assert.h>
+#include "libsapling/dm/queue.h"
+#include "libsapling/dm/typed/typed_common.h"
+#include "libsapling/cc/production.h"
+
+IMPLEMENT_TYPED_QUEUE(body, int, fpfdata_int)
+
+int main(void)
+{
+    // build production
+    node_t body = NULL;
+    body__insert(&body, 1);
+    body__insert(&body, 2);
+    body__insert(&body, 3);
+    production_t p = new_production(0, &body);
+
+    assert(production__id(p) == 0);
+    assert(production__len(p) == 3);
+    assert(*production__get(p, 0) == 1);
+    assert(*production__get(p, 1) == 2);
+    assert(*production__get(p, 2) == 3);
+}
